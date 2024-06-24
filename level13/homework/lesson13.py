f65685ecd5449083194d63db5bd32f684cf9081d
dgvepa1213 <erekledgvepadze@gmail.com>
2024-06-24 22:02:44 +0400

    commit

---
+++ level13/homework/lesson13.py
@@ -0,0 +1,46 @@
+#1) მომხმარებელს შემოატანინეთ 4 სხავდასხვა რიცხვი და ამ ოთხივე რიცხვზე მოახდინეთ არითმეტიკური ოპერაციები. +,-,*,//,**
+number = (input("enter your number"))
+number = (input("enter your number"))
+number = (input("enter your number"))
+number = (input("enter your number"))
+
+print(10 + 5)
+print(12 - 2)
+print(17 * 5)
+print(20 // 5)
+print(15 ** 7 )
+
+
+
+
+
+
+#2) დაწერეთ პროგრამა სადაც შემოიტანთ თქვენი ოჯახის წევრების ასაკს და გამოიტანეთ ის თუ რამდენი წლის იქნებიან 20 წლის შემდეგ.
+age = (input("Enter your age"))
+print("chemi deda aris 43 wlis da 20 wlis shemdeg iqneba 63 wlis")
+
+age = (input("Enter your age"))
+print("mamachemi aris 47 wlis da 20 wlis shemdeg iqneba 67 wlis")
+
+age = (input("Enter your age"))
+print("me var 12 wlis da 20 wlis shemdeg viqnebi 32 wlis")
+
+#3) შექმენით ცვლადები სადაც შეინახავთ თქვენს თავზე ინფორმაციას (სახელი, გვარი, ასაკი, ქვეყანა, ქალაქი, საყვარელი ფერი, საყვარელი მანქანა, საყვარელი საჭმელი, საყვარელი სპორტი და ა.შ) შემდეგ შემოტანილი მნიშვნელობებით ააწყვეტ ერთი დიდი წინადადება.
+name = "erekle"
+surname = "dgvephadze"
+age = 12
+country = "Georgia"
+city = "batumi"
+color = "red and blue"
+car = "bmw"
+food = "khinkali"
+sport = "box"
+
+#4)დაწერეთ მარტივი პროგრამა, რომელიც სთხოვს მომხმარებელს შეიყვანოს თავისი სახელი და შემდეგ დაბეჭდოს მისალოცი შეტყობინება მათი სახელის გამოყებით
+
+name = (input("enter your name"))
+print("gilocav dabadebis dges gisureb jamrtelobas warmatebas da bednierebas,siyvarulit erekle")
+
+
+
+
