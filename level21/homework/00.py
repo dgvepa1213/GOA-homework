d0639f640035ff19f15d26c31543e7844b4a6b47
dgvepa1213 <erekledgvepadze@gmail.com>
2024-08-19 22:13:35 +0400

    commit

---
+++ level21/homework/00.py
@@ -0,0 +1,28 @@
+# for i in range(126,-1,- 1):
+#     print(i)
+
+
+# for i in range(126,-1,-2):
+#     print(i)
+
+
+# for i in range(126,-1,-3):
+#     print(i)
+
+
+
+result=0
+for i in range(1,48):
+    result+=i
+    print(result)
+
+
+
+result=1
+for i in range(1,48):
+    result*=i
+    print(result)
+
+
+
+
---
+++ level24/homework/00.py
@@ -0,0 +1,15 @@
+for i in range(1,126):   
+    print("erekle")
+
+for i in range(10,79):
+    print("GOA best") 
+
+for i in range(240,-1,-2):
+    print(i)
+
+for i in range(150,-1,-5):
+    print(i)
+
+
+for i in range(1,200):
+    print("Shota best")               
